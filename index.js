26c8c338221d5465e7cd9f0fe82e301d5f76619e
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-10 15:08:00 +0200

    calculatrice simple

--- index.js
+++ index.js
@@ -1,22 +1,75 @@
 // Salutation personnalisée :
+// Écrivez un programme qui demande à l'utilisateur son prénom.
+// Affichez une salutation telle que "Bonjour, [prénom] !".
 
 const readline = require('readline');
 
 const rl = readline.createInterface(process.stdin, process.stdout)
-rl.question('Quel est votre prénom ? ', (prenom) => {
+/* rl.question('Quel est votre prénom ? ', (prenom) => {
     console.log(`Bonjour ${prenom} !`)
     rl.close();
-})
+}) */
 
-// Écrivez un programme qui demande à l'utilisateur son prénom.
-// Affichez une salutation telle que "Bonjour, [prénom] !".
 // Calculatrice simple :
-
 // Demandez à l'utilisateur deux nombres.
 // Demandez à l'utilisateur une opération (addition, soustraction, multiplication, division).
 // Affichez le résultat de l'opération.
-// Table de multiplication :
 
+const firstNumber = [];
+const secondNumber = [];
+const op = [];
+let r = [];
+
+
+rl.question('Donnez-moi un nombre', (nbr1) => {
+    firstNumber.push(nbr1)
+    rl.question('Donnez moi un deuxième nombre', (nbr2) => {
+        secondNumber.push(nbr2);
+        rl.question('Quelle sera l\'opérateur ?', (operator) => {
+            op.push(operator);
+
+            const num1 = parseFloat(firstNumber);
+            const num2 = parseFloat(secondNumber);
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2
+                    r.push(result);
+                    break;
+                case '-':
+                    result = num1 - num2
+                    r.push(result);
+                case '*':
+                    result = num1 * num2
+                    r.push(result);
+                case '/':
+                    if(num2 !== 0) {
+                        result = num1 / num2
+                        r.push(result);
+                    }else {
+                        console.log('Erreur division par 0 impossible')
+                        rl.close()
+                        return;
+                    }
+                    break;
+                default: 
+                    console.log('Opérateur non reconnu');
+                    rl.close();
+                    return;
+            }
+
+            console.log(`${firstNumber} ${op} ${secondNumber} = ${r}`)
+            rl.close();
+        })
+    })
+})
+
+
+
+
+
+
+// Table de multiplication :
 // Demandez à l'utilisateur un nombre.
 // Affichez la table de multiplication pour ce nombre de 1 à 10.
 // Palindrome :
