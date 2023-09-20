83ef939af4d1838ddcec80541027e537ca35b9f5
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-20 19:12:19 +0200

    Nombre Premier function version Optimale

--- NombrePremier.js
+++ NombrePremier.js
@@ -23,3 +23,38 @@ function nombrePremier(nbr){
 }
 
 console.log(nombrePremier(5))
+
+
+
+// Version Optimale
+
+function isPrime(nbr)  {
+
+    if(nbr <= 1){
+        return false;
+    }
+
+    if (nbr <= 3) {
+        return true
+    }
+
+    if(nbr % 2 === 0 || nbr % 3 === 0){
+        return false;
+    }
+
+
+    let i = 5;
+    while (i * i <= nbr) {
+        if(nbr % i === 0 ||nbr % (i + 2) === 0){
+            return false;
+        }
+
+        i += 6
+    }
+
+    return true;
+
+
+
+
+}
