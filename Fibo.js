eb56b60c004ec0248471e54e9024f2c627c22bd1
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-21 18:39:39 +0200

    fibo

--- Fibo.js
+++ Fibo.js
@@ -1,27 +1,34 @@
 // 1, 2, 3, 5, 8, 13, 21 et 34
 
-function Fibonacci(nbr){
+// function Fibonacci(nbr){
 
-    if(nbr === 1){
-        return nbr 
-    }
+//     if(nbr === 1){
+//         return nbr 
+//     }
 
-    let a = 0;
-    let b = 1;
+//     let a = 0;
+//     let b = 1;
  
-    for(let i = 2 ; i <= nbr ; i++ ){
+//     for(let i = 2 ; i <= nbr ; i++ ){
 
-        let temp = a + b
-        a = b
-        b = temp
+//         let temp = a + b
+//         a = b
+//         b = temp
          
 
 
+//     }
+
+//     return b;
+
+
+// }
+
+function Fibonacci(nbr) {
+    if (nbr <= 1) {
+        return nbr;
     }
-
-    return b;
-
-
+    return Fibonacci(nbr - 1) + Fibonacci(nbr - 2);
 }
 
-console.log(Fibonacci(8))
+console.log(Fibonacci(8)); // 21
