b4bad2fdf6db7a1197967277c75c2c2f9505a81b
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-21 17:43:14 +0200

    reduce min/max

--- Reduce.js
+++ Reduce.js
@@ -7,3 +7,12 @@ const x = [3, 5, 7, 9, 11].reduce((accumulateur, currentValue) => {
 // 2nd Call:        8              7           15
 // 3rd Call:        15             9           24
 // 4th Call:        24             11          35
+
+
+const num = [ 87, 64, 96, 92, 88, 99, 73]
+
+const max = num.reduce((max, currentValue) => {
+    return (currentValue > max) ? currentValue : max
+})
+
+console.log(max)
