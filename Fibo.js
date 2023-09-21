0af6630210a0187754be06d67e8da88aea24cfc0
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-21 18:28:02 +0200

    reduce part 2

--- Fibo.js
+++ Fibo.js
@@ -1,6 +1,27 @@
+// 1, 2, 3, 5, 8, 13, 21 et 34
+
 function Fibonacci(nbr){
 
-    
+    if(nbr === 1){
+        return nbr 
+    }
+
+    let a = 1;
+    let b = 0;
+ 
+    for(let i = 2 ; i < nbr ; i++ ){
+
+        let temp = a + b
+        a = b
+        b = temp
+         
+
+
+    }
+
+    return b
 
 
 }
+
+console.log(Fibonacci(5))
