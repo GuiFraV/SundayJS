94f918d694f127addba7e51cc7ed8bae54d447a8
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-23 13:48:12 +0200

    compteur .reduceJS

--- optiq.js
+++ optiq.js
@@ -1,38 +1,53 @@
-// 2, 3, 5, 7, 11, 13, 17, 19
-const isPrime = (nbr) => {
+// // 2, 3, 5, 7, 11, 13, 17, 19
+// const isPrime = (nbr) => {
 
-    if(nbr <= 1){
-        return false;
+//     if(nbr <= 1){
+//         return false;
+//     }
+
+//     if (nbr <= 3){
+//         return true;
+//     }
+
+//     if(nbr % 2 === 0 || nbr % 3 === 0){
+//         return false;
+//     }
+
+//     let i = 5;
+//     while(i * i <= nbr){
+
+//         if(nbr % i === 0 || nbr % (i + 2) === 0){
+
+//             return false;
+
+
+//         }
+//         i += 6;
+
+//     }
+
+//     return true
+
+
+
+// }
+
+// console.log(isPrime(5))
+// console.log(isPrime(7))
+// console.log(isPrime(11))
+// console.log(isPrime(4))
+
+
+const vote = ['y','y','y','n','y','y','y','n','y','y','y','n','n',,'n','n',,'n','n','n','n']
+const x = vote.reduce((compteur, currentValue) => {
+
+    if(compteur[currentValue]){
+        compteur[currentValue]++
+    }else{
+        compteur[currentValue] = 1
     }
 
-    if (nbr <= 3){
-        return true;
-    }
+    return compteur
+}, {})
 
-    if(nbr % 2 === 0 || nbr % 3 === 0){
-        return false;
-    }
-
-    let i = 5;
-    while(i * i <= nbr){
-
-        if(nbr % i === 0 || nbr % (i + 2) === 0){
-
-            return false;
-
-
-        }
-        i += 6;
-
-    }
-
-    return true
-
-
-
-}
-
-console.log(isPrime(5))
-console.log(isPrime(7))
-console.log(isPrime(11))
-console.log(isPrime(4))
+console.log(x);
