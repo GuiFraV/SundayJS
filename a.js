546f6f33c1f9db1043361e0785efd0ba519aa441
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-24 14:52:23 +0200

    nombre premier simple

--- a.js
+++ a.js
@@ -1,14 +1,41 @@
-const vote = ['y','y','y','n','y','y','y','n','y','y','y','n','n',,'n','n',,'n','n','n','n']
-const x = vote.reduce((compteur, curr) => {
+// const vote = ['y','y','y','n','y','y','y','n','y','y','y','n','n',,'n','n',,'n','n','n','n']
+// const x = vote.reduce((compteur, curr) => {
 
-    if(compteur[curr]){
-        compteur[curr]++
-    }else{
-        compteur[curr] = 1
-    }
+//     if(compteur[curr]){
+//         compteur[curr]++
+//     }else{
+//         compteur[curr] = 1
+//     }
 
-    return compteur;
+//     return compteur;
 
-}, {});
+// }, {});
 
-console.log(x);
+// console.log(x);
+
+// 2, 3, 5, 7, 11, 13, 17, 19
+
+// const isPrime = (nbr) => {
+    
+//     if(nbr <= 1){
+//         return false;
+//     }
+
+//     for(let i = 2; i<nbr; i++){
+
+//         if( nbr % i === 0){
+//             return false;
+//         }
+
+
+//     }
+
+//     return true;
+
+
+
+// }
+
+// console.log(isPrime(2))
+// console.log(isPrime(3))
+// console.log(isPrime(4))
