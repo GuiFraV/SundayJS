0eb635ee098d7e223588a18bcbdfb6adf2efcdb7
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-28 11:26:18 +0200

    revision

--- c.js
+++ c.js
@@ -15,40 +15,49 @@
 // console.log(res)
 
 // 2, 3, 5, 7, 11, 13, 17, 19
-const isPrime = (nbr) => {
+// const isPrime = (nbr) => {
 
-    if(nbr <= 1){
-        return false;
-    }
+//     if(nbr <= 1){
+//         return false;
+//     }
 
-    if(nbr <= 3){
-        return true;
-    }
+//     if(nbr <= 3){
+//         return true;
+//     }
 
-    if(nbr % 2 === 0 || nbr % 3 === 0 ){
-        return false;
-    }
+//     if(nbr % 2 === 0 || nbr % 3 === 0 ){
+//         return false;
+//     }
 
-    let i = 5;
-    while(i * i <= nbr){
+//     let i = 5;
+//     while(i * i <= nbr){
 
-        if(nbr % i === 0 || nbr % (i + 2) === 0){
-            return false;
-        }
-        i += 6
+//         if(nbr % i === 0 || nbr % (i + 2) === 0){
+//             return false;
+//         }
+//         i += 6
 
-    }
+//     }
 
-    return true;
+//     return true;
 
 
-}
+// }
 
-console.log(isPrime(2))
-console.log(isPrime(3))
-console.log(isPrime(4))
-console.log(isPrime(5))
-console.log(isPrime(6))
-console.log(isPrime(7))
-console.log(isPrime(8))
-console.log(isPrime(11))
+// console.log(isPrime(2))
+// console.log(isPrime(3))
+// console.log(isPrime(4))
+// console.log(isPrime(5))
+// console.log(isPrime(6))
+// console.log(isPrime(7))
+// console.log(isPrime(8))
+// console.log(isPrime(11))
+
+const num = [ 87, 64, 96, 92, 88, 99, 73]
+const x = num.reduce((acc, curr) => {
+    
+    return (curr > acc) ? curr : acc;
+
+})
+
+console.log(x)
