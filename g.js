dd3d8542106a99a8fe46e690a3a9805a064c7baf
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-04 14:06:00 +0200

    revision

---
+++ g.js
@@ -0,0 +1,54 @@
+// // 2, 3, 5, 7, 11, 13, 17, 19
+
+// const isPrime = (nbr) => {
+
+//     if(nbr <= 1){
+//         return false;
+//     }
+
+//     if(nbr <= 3){
+//         return true;
+//     }
+
+//     if(nbr % 2 === 0 || nbr % 3 === 0){
+//         return false;
+//     }
+
+
+//     let i = 5;
+
+//     while(i * i <= nbr){
+
+//         if(nbr % i === 0 || nbr % (i+2) === 0){
+
+//             return false;
+
+//         }
+//         i+= 6
+
+
+//     }
+
+//     return true;
+
+
+// }
+
+// console.log(isPrime(1))
+// console.log(isPrime(2))
+// console.log(isPrime(3))
+// console.log(isPrime(4))
+// console.log(isPrime(5))
+// console.log(isPrime(6))
+// console.log(isPrime(7))
+// console.log(isPrime(8))
+// console.log(isPrime(9))
+// console.log(isPrime(10))
+// console.log(isPrime(11))
+// console.log(isPrime(12))
+// console.log(isPrime(13))
+// console.log(isPrime(14))
+// console.log(isPrime(15))
+// console.log(isPrime(16))
+// console.log(isPrime(17))
+// console.log(isPrime(18))
