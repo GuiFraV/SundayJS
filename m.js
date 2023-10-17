664f36978a9912fe2759f99351d6cdd734fc7acf
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-17 10:54:19 +0200

    revision occurence with forEach Method

--- m.js
+++ m.js
@@ -1,15 +1,36 @@
 const v = ['y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','n','n','y','n','n','y','n','n','y','n','n','y','n','n','y','y','y']
 
-const x = v.reduce((acc, curr) => {
+// With reduce method
+// const x = v.reduce((acc, curr) => {
 
-    if(acc[curr]){
-        acc[curr] += 1
-    }else{
-        acc[curr] = 1
-    }
+//     if(acc[curr]){
+//         acc[curr] += 1
+//     }else{
+//         acc[curr] = 1
+//     }
 
-    return acc;
+//     return acc;
 
-},{})
+// },{})
 
-console.log(x)
+// With ForEach Method
+function countOccurence(arr){
+
+    let obj = {};
+
+    v.forEach((a) => {    
+    
+        if(obj[a]){
+            obj[a] += 1;
+        }else{
+            obj[a] = 1
+        }
+    
+        // console.log(obj)
+        
+    })
+    return obj;
+}
+
+
+console.log(countOccurence(v))
