3c4213e653999de764e11ae7b42dd88c7ce9c87a
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-18 10:21:32 +0200

    isPrime Opti vers

---
+++ o.js
@@ -0,0 +1,29 @@
+// 2, 3, 5, 7, 11, 13, 17, 19
+
+function isPrime(nbr){
+
+    if(nbr <= 1){
+        return false;
+    }
+
+
+    for(let i = 2; i< nbr; i++){
+
+        if(nbr % i === 0){
+            return false;
+        }
+
+    }
+
+    return true;
+
+
+}
+
+console.log(isPrime(1))
+console.log(isPrime(2))
+console.log(isPrime(3))
+console.log(isPrime(4))
+console.log(isPrime(5))
+console.log(isPrime(6))
+console.log(isPrime(7))
