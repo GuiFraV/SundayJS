50ef2dbeca85f448b60af278d36cb61696ac7212
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-20 21:42:37 +0200

    tournament algo team revision

--- q.js
+++ q.js
@@ -1,26 +1,75 @@
-const array = [5, 1, 22, 25, 6, -1, 8, 10]
-const sequence = [1, 6, -1, 10]
+// const array = [5, 1, 22, 25, 6, -1, 8, 10]
+// const sequence = [1, 6, -1, 10]
 
-function validateSubsequence(array, sequence){
+// function validateSubsequence(array, sequence){
 
-    let arrIdx = 0;
-    let seqIdx = 0;
+//     let arrIdx = 0;
+//     let seqIdx = 0;
 
-    while (arrIdx < array.length && seqIdx < sequence.length){
+//     while (arrIdx < array.length && seqIdx < sequence.length){
 
-        if(array[arrIdx] !== sequence[seqIdx]){
+//         if(array[arrIdx] !== sequence[seqIdx]){
 
-            arrIdx++
+//             arrIdx++
+
+//         }else{
+
+//             seqIdx++
+//         }
+
+//     }
+
+//     return seqIdx === sequence.length
+
+// }
+
+// console.log(validateSubsequence(array,sequence))
+
+
+const competions = [
+    ['HTML', 'C#'],
+    ['C#', "Python"],
+    ["Python", "HTML"]
+]
+const results = [0, 0 ,1]
+
+function resultsTournament(teams, result){
+
+    let winning = {};
+
+    for(let i = 0; i < teams.length; i++){
+
+        let homeTeam = teams[i][0];
+        let awayTeam = teams[i][1];
+
+        if(results[i] === 0){
+
+            if(winning[awayTeam]){
+                winning[awayTeam]+=3
+            }else{
+                winning[awayTeam] = 3
+            }
 
         }else{
 
-            seqIdx++
+            if(winning[homeTeam]){
+                winning[homeTeam]+=3
+            }else{
+                winning[homeTeam] = 3
+            }
         }
+
 
     }
 
-    return seqIdx === sequence.length
+    const count = Object.entries(winning).sort((a,b) => b[1] - a[1])
+    let winningTeam = count[0][0]
+
+    return winningTeam;
+
+
+
 
 }
 
-console.log(validateSubsequence(array,sequence))
+console.log(resultsTournament(competions, results))
