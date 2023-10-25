aff9f5ebfb21550e318a205accc6bcb14511e1b1
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-25 16:30:06 +0200

    isPrime simplified vers revision

--- 7.js
+++ 7.js
@@ -1,26 +1,54 @@
-const num = [ 87, 64, 96, 92, 88, 99, 73]
-const x = num.reduce((acc, curr) => {
-    return (acc > curr) ? acc : curr
-})
+// const num = [ 87, 64, 96, 92, 88, 99, 73]
+// const x = num.reduce((acc, curr) => {
+//     return (acc > curr) ? acc : curr
+// })
 
-console.log(x)
+// console.log(x)
 
 
-const y = (arr) => {
+// const y = (arr) => {
 
-    let min = 0;
+//     let min = 0;
 
-    for(let i = 0; i< arr.length; i++){
+//     for(let i = 0; i< arr.length; i++){
 
-        if(min < arr[i]){
+//         if(min < arr[i]){
             
-            min = arr[i]
+//             min = arr[i]
+//         }
+
+//     }
+
+//     return min;
+
+// }
+
+// console.log(y(num))
+
+// 2, 3, 5, 7, 11, 13, 17, 19 
+
+const isPrime = (nbr)=> {
+
+    if(nbr <= 1){
+        return false;
+    }
+
+    for(let i = 2; i< nbr; i++){
+
+        if(nbr % i === 0){
+            return false;
         }
 
     }
 
-    return min;
-
+    return true;
 }
 
-console.log(y(num))
+console.log(isPrime(1))
+console.log(isPrime(2))
+console.log(isPrime(3))
+console.log(isPrime(4))
+console.log(isPrime(5))
+console.log(isPrime(6))
+console.log(isPrime(7))
+console.log(isPrime(8))
