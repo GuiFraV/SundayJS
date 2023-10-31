0c7b6cd895f2ae07461dab18e7519069471ed9ef
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-31 12:57:16 +0100

    revision algo subsequence

--- 13.js
+++ 13.js
@@ -70,3 +70,31 @@ function winningTeam(team, results){
 }
 
 console.log(winningTeam(competitions, results))
+
+
+const array = [5, 1, 22, 25, 6, -1, 8, 10]
+const sequence = [1, 6, -1, 10, 25]
+
+
+function validateSubsequence(arr, seq){
+
+    let arrIdx = 0;
+    let seqIdx = 0;
+
+    while(arrIdx < arr.length && seqIdx < seq.length){
+
+        if(arr[arrIdx] !== seq[seqIdx]){
+
+            arrIdx++
+        }else{
+
+            seqIdx++
+        }
+
+    }
+
+    return seqIdx === seq.length
+
+}
+
+console.log(validateSubsequence(array, sequence))
