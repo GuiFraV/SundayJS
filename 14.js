7dbbeeb9d1dcd92659d5ae3b26a3ff345c935c45
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-11-01 15:12:38 +0100

    revision algo compter le nombre de fois

--- 14.js
+++ 14.js
@@ -1,5 +1,22 @@
 const num = [ 5, 10, 1, 6, 99, 22, 3, 2, 51, 98]
 const x = num.sort((a,b) => {return a - b})
 
-console.log(x);
+// console.log(x);
 
+const v = ['y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','n','n','y','n','n','y','n','n','y','n','n','y','n','n','y','y','y']
+const s = v.reduce((acc, curr) => {
+
+    if(acc[curr]){
+
+        acc[curr]++
+
+    }else{
+
+        acc[curr] = 1
+    }
+
+    return acc;
+
+}, {})
+
+console.log(s)
