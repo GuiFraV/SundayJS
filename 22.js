2ef2f59a44b24ae21d50f2aee87a7287957f2845
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-11-08 10:03:03 +0100

    revision subsequence array

---
+++ 22.js
@@ -0,0 +1,27 @@
+const array = [5, 1, 22, 25, 6, -1, 8, 10]
+const sequence = [1, 6, -1, 10, 12]
+
+function isSubsequence(arr, seq){
+
+    let arrIdx = 0;
+    let seqIdx = 0;
+
+    while(arrIdx < arr.length && seqIdx < seq.length){
+
+        if(arr[arrIdx] !== seq[seqIdx]){
+
+            arrIdx++
+
+        }else{
+
+            seqIdx++
+        }
+
+    }
+
+    return seqIdx === seq.length;
+
+
+}
+
+console.log(isSubsequence(array, sequence))
