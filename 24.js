477d94bf1acd89d784a7fb44bc87290a75aa22fb
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-11-12 13:00:28 +0100

    revision coins changemin algo

--- 24.js
+++ 24.js
@@ -1,6 +1,20 @@
-const a = [7,10,23,5,8,9,56,82,66,77,87]
-const x = a.reduce((acc, curr) => {
-    return acc+curr;
-})
+// const a = [7,10,23,5,8,9,56,82,66,77,87]
+// const x = a.reduce((acc, curr) => {
+//     return acc+curr;
+// })
 
-console.log(x);
+// console.log(x);
+
+const coins = [5, 7, 1, 1, 2, 3, 22];
+const minChange = (arr) => {
+    let changeMin = 1;
+    let coinsSort = arr.sort((a,b) => a - b)
+    for(let i = 0 ; i<=coinsSort.length; i++){
+        if(changeMin >= coinsSort[i]){
+            changeMin += coinsSort[i]
+        }
+    }
+    return changeMin;
+}
+
+console.log(minChange(coins))
