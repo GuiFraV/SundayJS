0a7d40b88637b570ea928eb21a8fc9d092fdba11
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-11-17 11:37:02 +0100

    algo validatesubsequenc

--- algo3.js
+++ algo3.js
@@ -1,21 +1,46 @@
-const matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
+// const matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6]
+// ]
 
-const transposed = (m) => {
-    let rows = m.length;
-    let cols = m[0].length;
-    let transpo = [];
+// const transposed = (m) => {
+//     let rows = m.length;
+//     let cols = m[0].length;
+//     let transpo = [];
 
-    for(let j = 0 ; j < cols ; j++){
-        transpo[j] = [];
-        for(let i = 0 ; i < rows; i++){
-            transpo[j][i] = m[i][j]
+//     console.log(cols)
+
+//     for(let j = 0 ; j < cols ; j++){
+//         transpo[j] = [];
+
+//         console.log(transpo[j])
+//         for(let i = 0 ; i < rows; i++){
+//             transpo[j][i] = m[i][j]
+//             console.log(transpo[j][i])
+//         }
+//     }
+
+//     return transpo;
+// }
+
+// console.log(transposed(matrix));
+
+const array = [5, 1, 22, 25, 6, -1, 8, 10];
+const sequence = [1, 6, -1, 10, 11];
+
+function validateSub(arr, seq){
+
+    let arrIdx = 0;
+    let seqIdx = 0;
+
+    while(arrIdx < arr.length && seqIdx < seq.length){
+        if(arr[arrIdx] !== seq[seqIdx]){
+            arrIdx++
+        }else{
+            seqIdx++
         }
     }
-
-    return transpo;
+    return seqIdx === seq.length;
 }
 
-console.log(transposed(matrix));
+console.log(validateSub(array, sequence))
