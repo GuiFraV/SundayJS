5e8d3d46380fc2c41e127360b503d72db1b30a84
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-11-20 12:07:04 +0100

    algo fibonacci recursive

--- 27.js
+++ 27.js
@@ -1,14 +1,38 @@
-const coins = [5, 7, 1, 1, 2, 3, 22]
+// const coins = [5, 7, 1, 1, 2, 3, 22]
 
-const x = (arr) => {
-    let changeMin = 1;
-    let sortCoins = arr.sort((a,b) => {return  a - b})
-    for(let i = 0; i <= arr.length; i++){
-        if(changeMin >= sortCoins[i]){
-            changeMin += sortCoins[i]
-        }
+// const x = (arr) => {
+//     let changeMin = 1;
+//     let sortCoins = arr.sort((a,b) => {return  a - b})
+//     for(let i = 0; i <= arr.length; i++){
+//         if(changeMin >= sortCoins[i]){
+//             changeMin += sortCoins[i]
+//         }
+//     }
+//     return changeMin;
+// }
+
+// console.log(x(coins))
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+
+const fibo = (nbr) => {
+
+
+    if(nbr <= 1){
+        return nbr;
     }
-    return changeMin;
+
+    return fibo(nbr - 1) + fibo(nbr - 2);
 }
 
-console.log(x(coins))
+console.log(fibo(1))
+console.log(fibo(2))
+console.log(fibo(3))
+console.log(fibo(5))
+console.log(fibo(8))
+console.log(fibo(13))
+console.log(fibo(21))
+console.log(fibo(34))
+console.log(fibo(10))
+
+
