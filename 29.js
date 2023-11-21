6d9e5949098e8820de8c68d3ced53973afe50122
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-11-21 15:58:06 +0100

    algo revision compteur

---
+++ 29.js
@@ -0,0 +1,14 @@
+const v = ['y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','n','n','y','n','n','y','n','n','y','n','n','y','n','n','y','y','y']
+const x = v.reduce((acc, curr) => {
+
+    if(acc[curr]){
+        acc[curr]++;
+    }else{
+        acc[curr] = 1;
+    }
+
+    return acc;
+
+},{})
+
+console.log(x)
