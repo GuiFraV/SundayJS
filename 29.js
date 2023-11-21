7ac0473ce858dba9feaa7cb3e3c2dd5bd813dc15
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-11-21 16:00:20 +0100

    fibo récursif algo revision

--- 29.js
+++ 29.js
@@ -1,14 +1,34 @@
-const v = ['y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','n','n','y','n','n','y','n','n','y','n','n','y','n','n','y','y','y']
-const x = v.reduce((acc, curr) => {
+// const v = ['y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','n','n','y','n','n','y','n','n','y','n','n','y','n','n','y','y','y']
+// const x = v.reduce((acc, curr) => {
 
-    if(acc[curr]){
-        acc[curr]++;
-    }else{
-        acc[curr] = 1;
+//     if(acc[curr]){
+//         acc[curr]++;
+//     }else{
+//         acc[curr] = 1;
+//     }
+
+//     return acc;
+
+// },{})
+
+// console.log(x)
+
+const fibo = (nbr) => {
+
+    if(nbr <= 1){
+        return nbr;
     }
 
-    return acc;
+    return fibo(nbr - 1 ) + fibo(nbr - 2 );
 
-},{})
+}
 
-console.log(x)
+console.log(fibo(1))
+console.log(fibo(2))
+console.log(fibo(3))
+console.log(fibo(4))
+console.log(fibo(5))
+console.log(fibo(6))
+console.log(fibo(7))
+console.log(fibo(8))
+console.log(fibo(9))
