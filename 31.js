3650c405c92c3ee5a0e8bebe7c81e68672638134
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-11-23 16:09:56 +0100

    algo fibo récursif

---
+++ 31.js
@@ -0,0 +1,22 @@
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ...
+
+const fibo = (nbr) => {
+
+    if(nbr <= 1){
+        return nbr
+    }
+
+    return fibo(nbr - 1) + fibo(nbr - 2) 
+
+}
+
+console.log(fibo(1))
+console.log(fibo(2))
+console.log(fibo(3))
+console.log(fibo(4))
+console.log(fibo(5))
+console.log(fibo(6))
+console.log(fibo(7))
+console.log(fibo(8))
+console.log(fibo(9))
+console.log(fibo(10))
