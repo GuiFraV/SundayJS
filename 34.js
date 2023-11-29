9a3c2ae9b571ce99bedecf36b1adba105c971d1b
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-11-29 10:12:06 +0100

    fibo itératif algo

---
+++ 34.js
@@ -0,0 +1,24 @@
+const fibo = (nbr) => {
+
+    if(nbr <= 1){
+        return nbr;
+    }
+
+    let a = 0;
+    let b = 1;
+
+    for(let i = 0; i< nbr ; i++){
+        let res = a + b;
+        a = b;
+        b = res;
+    }
+    return b;
+}
+
+console.log(fibo(1))
+console.log(fibo(2))
+console.log(fibo(3))
+console.log(fibo(4))
+console.log(fibo(5))
+console.log(fibo(6))
+console.log(fibo(7))
