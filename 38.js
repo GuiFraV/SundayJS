d7f07c1524151775dde2a8d82cb3083bfbac3684
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-12-06 09:32:05 +0100

    revision algo

--- 38.js
+++ 38.js
@@ -1,61 +1,80 @@
-// const isPrime = (nbr) => {
+// // const isPrime = (nbr) => {
 
 
-//     if(nbr <= 1){
-//         return false;
-//     }
+// //     if(nbr <= 1){
+// //         return false;
+// //     }
 
-//     for(let i = 2; i < nbr ; i++){
+// //     for(let i = 2; i < nbr ; i++){
 
-//         if(nbr % i === 0 ){
-//             return false;
+// //         if(nbr % i === 0 ){
+// //             return false;
+// //         }
+
+// //     }
+
+// //     return true;
+
+// // }
+
+
+// // console.log(isPrime(1))
+// // console.log(isPrime(2))
+// // console.log(isPrime(3))
+// // console.log(isPrime(4))
+// // console.log(isPrime(5))
+// // console.log(isPrime(6))
+// // console.log(isPrime(8))
+
+
+
+
+
+// const matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6]
+// ]
+
+
+// const transposedMatrix = (arr) => {
+
+
+//     let rows = arr.length;
+//     let cols = arr[0].length;
+//     let transposed = [];
+
+
+//     for(let j = 0 ; j < cols; j++){
+//         transposed[j] = [];
+//         for(let i = 0 ; i < rows; i++){
+//             transposed[j][i] = arr[i][j];
 //         }
 
 //     }
 
-//     return true;
+//     return transposed;
 
 // }
 
 
-// console.log(isPrime(1))
-// console.log(isPrime(2))
-// console.log(isPrime(3))
-// console.log(isPrime(4))
-// console.log(isPrime(5))
-// console.log(isPrime(6))
-// console.log(isPrime(8))
+
+// console.log(transposedMatrix(matrix))
 
 
+const fibo = (nbr) => {
 
-
-
-const matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
-
-
-const transposedMatrix = (arr) => {
-
-
-    let rows = arr.length;
-    let cols = arr[0].length;
-    let transposed = [];
-
-
-    for(let j = 0 ; j < cols; j++){
-        transposed[j] = [];
-        for(let i = 0 ; i < rows; i++){
-            transposed[j][i] = arr[i][j];
-        }
-
+    if(nbr <= 1){
+        return nbr;
     }
 
-    return transposed;
+    return fibo(nbr - 1 ) + fibo(nbr - 2 );
 
 }
 
-
-
-console.log(transposedMatrix(matrix))
+console.log(fibo(1))
+console.log(fibo(2))
+console.log(fibo(3))
+console.log(fibo(4))
+console.log(fibo(5))
+console.log(fibo(7))
+console.log(fibo(8))
