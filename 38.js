d5d978999ee495ca726bc0ac9821097b7fea5ded
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-12-06 09:25:02 +0100

    rev algo

---
+++ 38.js
@@ -0,0 +1,27 @@
+// const isPrime = (nbr) => {
+
+
+//     if(nbr <= 1){
+//         return false;
+//     }
+
+//     for(let i = 2; i < nbr ; i++){
+
+//         if(nbr % i === 0 ){
+//             return false;
+//         }
+
+//     }
+
+//     return true;
+
+// }
+
+
+// console.log(isPrime(1))
+// console.log(isPrime(2))
+// console.log(isPrime(3))
+// console.log(isPrime(4))
+// console.log(isPrime(5))
+// console.log(isPrime(6))
+// console.log(isPrime(8))
