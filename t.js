c2daa7e7c55a7fc7e019e548997035be149adfe1
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-12-13 20:48:42 +0100

    algo test 3

--- t.js
+++ t.js
@@ -36,3 +36,34 @@ const valide = (arr, seq) => {
 }
 
 console.log(valide(array, sequence))
+
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+
+const transposed = (arr) => {
+
+    let rows = arr.length;
+    let cols = arr[0].length;
+    let t = [];
+
+    for(let j = 0 ; j < cols; j++){
+
+        t[j] = [];
+
+        for(let i = 0 ; i < rows; i++){
+
+            t[j][i] = arr[i][j]
+
+
+        }
+
+    }
+
+    return t;
+
+
+}
+
+console.log(transposed(matrix))
