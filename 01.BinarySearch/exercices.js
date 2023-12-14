8dd9e8e93ad1aab6302d491e86ce9cf200c10d70
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-12-14 11:17:27 +0100

    exercice binary Search

--- 01.BinarySearch/exercices.js
+++ 01.BinarySearch/exercices.js
@@ -53,4 +53,70 @@ function binarySearch(olist, target){
 
 }
 
-console.log(binarySearch(liste, 11))
+// console.log(binarySearch(liste, 11))
+
+
+
+/*
+
+Exercice : Trouver le Point de Changement dans une Liste Triée en Rotation
+
+Imaginez que vous avez une liste triée d'entiers qui a été "rotée" à un certain point. Par exemple, si la liste triée originale est [1, 2, 3, 4, 5, 6] et qu'elle est rotée à partir de l'index 3, elle devient [4, 5, 6, 1, 2, 3]. Votre tâche est de trouver l'index où cette rotation a eu lieu en utilisant la recherche binaire.
+
+Instructions :
+
+Écrivez une fonction en JavaScript qui prend une liste triée en rotation en tant qu'entrée.
+Utilisez la recherche binaire pour trouver l'index du "point de changement", c'est-à-dire l'endroit où l'élément suivant est inférieur à l'élément actuel.
+Retournez l'index de cet élément. Si la liste n'est pas rotée (c'est-à-dire qu'elle est triée normalement), retournez 0.
+Exemple d'entrée et de sortie :
+
+Entrée : liste = [4, 5, 6, 1, 2, 3]
+Sortie : 3 (car l'élément à l'index 3 est 1, qui est inférieur à 6 à l'index 2)
+
+*/
+
+const l = [4, 5, 6, 1, 2, 3]
+// const l = [1, 2, 3, 4, 5, 6]
+
+// const rotation = (liste) => {
+//     let low = 0;
+//     let high = liste.length -1;
+//     while(low <= high){
+//         let mid = Math.floor((low + high)/2);
+//         let item = liste[mid];
+//         let rotate = mid + 1;
+      
+//         if(item > liste[rotate]){
+//             return rotate;
+//         }else{
+//             return 0;
+//         }
+//     }
+// }
+
+// console.log(rotation(l));
+
+// Correction : 
+
+const rotation = (liste) => {
+
+
+    let low = 0;
+    let high = liste.length -1
+
+    while (low <= high){
+
+        let mid = Math.floor((low + high) / 2);
+
+        if(liste[mid] > liste[mid +1]){
+            return mid + 1
+        }else if(liste[low] < liste[mid]){
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+        }
+    }
+    return 0;
+}
+
+console.log(rotation(l))
