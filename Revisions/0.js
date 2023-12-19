71c63dac4b6e880afbdb11c8a967c8a281f33946
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-12-19 12:00:50 +0100

    algo exercice

--- Revisions/0.js
+++ Revisions/0.js
@@ -1,22 +1,39 @@
-const array = [5, 1, 22, 25, 6, -1, 8, 10];
-const sequence = [1, 6, -1, 10];
+// const array = [5, 1, 22, 25, 6, -1, 8, 10];
+// const sequence = [1, 6, -1, 10];
 
-const validate = (arr, sub) => {
+// const validate = (arr, sub) => {
 
-    let arrIdx = 0;
-    let subIdx = 0;
+//     let arrIdx = 0;
+//     let subIdx = 0;
 
-    while(arrIdx < arr.length && subIdx < sub.length){
+//     while(arrIdx < arr.length && subIdx < sub.length){
 
-        if(arr[arrIdx] !== sub[subIdx]){
-            arrIdx++;
-        }else{
-            subIdx++;
-        }
+//         if(arr[arrIdx] !== sub[subIdx]){
+//             arrIdx++;
+//         }else{
+//             subIdx++;
+//         }
+//     }
+//     return subIdx === sub.length;
+
+// }
+
+
+// console.log(validate(array, sequence));
+
+
+const v = ['y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','y','n','n','y','n','n','y','n','n','y','n','n','y','n','n','y','n','n','y','y','y']
+
+const x = v.reduce((acc, cur) =>{
+
+    if(acc[cur]){
+        acc[cur]++
+    }else{
+        acc[cur] = 1;
     }
-    return subIdx === sub.length;
 
-}
+    return acc;
 
+}, {});
 
-console.log(validate(array, sequence));
+console.log(x)
