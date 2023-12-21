310cad8d54c81aed3cfc645574f764e49149596a
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-12-21 16:17:52 +0100

    algo exercice

--- Revisions/yo.js
+++ Revisions/yo.js
@@ -21,4 +21,43 @@ const transposed = (arr) => {
     return t;
 }
 
-console.log(transposed(matrix));
+// console.log(transposed(matrix));
+
+const optiPrimeNumber = (nbr) => {
+
+
+    if(nbr <= 1){
+        return false;
+    }
+
+    if(nbr <= 3){
+        return true;
+    }
+
+    if(nbr % 2 === 0 || nbr % 3 === 0){
+        return false;
+    }
+
+    let i = 5;
+
+    while(i * i <= nbr){
+
+        if(nbr % i === 0 || nbr % (i + 2) === 0){
+            return false;
+            i += 6;
+        }
+    }
+
+    return true;
+
+} 
+
+console.log(optiPrimeNumber(1))
+console.log(optiPrimeNumber(2))
+console.log(optiPrimeNumber(3))
+console.log(optiPrimeNumber(4))
+console.log(optiPrimeNumber(5))
+console.log(optiPrimeNumber(6))
+console.log(optiPrimeNumber(7))
+console.log(optiPrimeNumber(8))
+console.log(optiPrimeNumber(9))
