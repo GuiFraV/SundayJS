32816ff501df920e174143e94cf36078ae8309a0
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-01-06 09:41:01 +0100

    algo revision

--- Revisions/z.js
+++ Revisions/z.js
@@ -83,4 +83,30 @@ const findClosestValue = (tree, target) => {
     return closest;
 }
 
-console.log(findClosestValue(bstRoot, 12))
+console.log(findClosestValue(bstRoot, 12));
+
+const array = [5, 1, 22, 25, 6, -1, 8, 10]
+const sequence = [1, 6, -1, 10]
+
+const validateSubSequence = (arr, seq) => {
+    let arrIdx = 0;
+    let seqIdx = 0;
+
+    while(arrIdx < arr.length && seqIdx < seq.length){
+
+        if(arr[arrIdx] !== seq[seqIdx]){
+
+            arrIdx++
+
+        }else{
+            seqIdx++
+        }
+
+
+    }
+
+    return seqIdx === seq.length;
+
+}
+
+console.log(validateSubSequence(array,sequence))
